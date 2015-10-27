d485af0c434cf21246b02a2ec6128762dcc4d0cc
Janko Thyson <janko.thyson@rappster.de>
2015-10-27 18:35:24 +0100

    exported createListIndex

--- R/code.R
+++ R/code.R
@@ -1,6 +1,7 @@
 
 # Create list index -------------------------------------------------------
 
+#' @export
 createListIndex <- function(x, sep = c("[['", "']][['", "']]")) {
   paste0(sep[1], paste(x, collapse = sep[2]), sep[3])
 }
